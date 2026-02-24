114cb0cc7ef0a54eca696a5af2bebdd4bc381d9a
ad-tsn <ademm.tsnl@gmail.com>
2026-02-24 22:10:59 +0100

    feat: add room booking app with models and API structure

--- backend/booking_backend/room_booking/models.py
+++ backend/booking_backend/room_booking/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    ROOM_TYPES = [
+        ('suite', 'Suite'),
+        ('standard', 'Chambre Standard'),
+        ('deluxe', 'Chambre Deluxe'),
+    ]
+    CURRENCY_TYPES = [
+        ('USD', 'USD'),
+        ('EUR', 'EUR'),
+    ]
+    name = models.CharField(max_length=100, blank=True, default='')
+    type = models.CharField(max_length=100,choices=ROOM_TYPES)
+    pricePerNight = models.IntegerField(default=150)
+    currency = models.CharField(default="EUR", max_length=10, choices=CURRENCY_TYPES)
+    maxOccupancy = models.IntegerField(default=1)
+    description = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return f"{self.name} ({self.type})"
