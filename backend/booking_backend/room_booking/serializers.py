e2a13759dc60736fda9cb87cc495302e906f7103
ad-tsn <ademm.tsnl@gmail.com>
2026-02-25 19:31:09 +0100

    feat: add occupied dates model and API endpoints

--- backend/booking_backend/room_booking/serializers.py
+++ backend/booking_backend/room_booking/serializers.py
@@ -1,7 +1,26 @@
 from rest_framework import serializers
-from .models import Room
+from .models import Room,RoomImage,OccupiedDate
+
+class RoomImageSerializer(serializers.ModelSerializer):
+    room = serializers.HyperlinkedRelatedField(
+        view_name = 'room-detail',
+        queryset = Room.objects.all()
+    ),
+    class Meta :
+        model = RoomImage
+        fields = ['id', 'image', 'caption', 'room']
 
 class RoomSerializer(serializers.HyperlinkedModelSerializer):
+    images = RoomImageSerializer(many=True, read_only=True)
     class Meta :
         model = Room
-        fields = ['url', 'id', 'name', 'type', 'pricePerNight', 'currency', 'maxOccupancy', 'description']
+        fields = ['url', 'id', 'name', 'type', 'pricePerNight', 'currency', 'maxOccupancy', 'description', 'images']
+
+class OccupiedDateSerializer(serializers.HyperlinkedModelSerializer):
+    room = serializers.HyperlinkedRelatedField(
+        view_name = 'room-detail',
+        queryset = Room.objects.all()
+    ),
+    class Meta:
+        model = OccupiedDate
+        fields = ['url', 'id', 'room', 'date']
--- backend/booking_backend/room_booking/urls.py
+++ backend/booking_backend/room_booking/urls.py
@@ -5,7 +5,15 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     path('',views.api_root,name="api_root"),
     path('rooms/',views.RoomList.as_view(),name="room-list"),
-    path("rooms/<int:pk>/",views.RoomDetail.as_view(),name="room-detail")
+    path("rooms/<int:pk>/",views.RoomDetail.as_view(),name="room-detail"),
+    path('occupied-dates/',views.OccupiedDatesList.as_view(),name="occupieddate-list"),
+    path('occupied-dates/<int:pk>/',views.OccupiedDatesDetail.as_view(),name="occupieddate-detail")
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG: 
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
